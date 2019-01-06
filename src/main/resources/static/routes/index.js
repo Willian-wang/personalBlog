c65b2270df467344e5994a2e430811750123b500
Willian-wang <a007a007a@sina.cn>
2019-01-07 00:50:15 +0800

    添加路由与package.json,app.js等文件

---
+++ src/main/resources/static/routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path')
+var app = require('../app')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/login',function (req,res,next) {
+    res.sendFile(process.cwd() +'/public/login.html');
+});
+
+
+router.get('/resign',function (req,res,next) {
+    res.sendFile(process.cwd() +'/public/resign.html');
+     });
+
+router.get()
+module.exports = router;
+
+
